1c5989422a985bb12cb54ba28dd2ac69ab5b77f3
vikas-gandham <gandhamvikas@gmail.com>
2024-05-17 18:51:46 +0530

    added activeVariants to the product and added custimized navigation buttons to the swiper

--- components/home/productCarousel.js
+++ components/home/productCarousel.js
@@ -1,5 +1,6 @@
 import Product from "@/components/reusable/product";
-import { useState } from "react";
+import { useRef, useState } from "react";
+import { GoChevronLeft, GoChevronRight } from "react-icons/go";
 import { Swiper, SwiperSlide } from "swiper/react";
 
 import { Navigation } from "swiper/modules";
@@ -7,152 +8,356 @@ import "swiper/css";
 import "swiper/css/navigation";
 
 export default function ProductCarousal() {
+  const swiperRef = useRef(null);
+
+  const handlePrev = () => {
+    if (swiperRef.current && swiperRef.current.swiper) {
+      swiperRef.current.swiper.slidePrev();
+    }
+  };
+
+  const handleNext = () => {
+    if (swiperRef.current && swiperRef.current.swiper) {
+      swiperRef.current.swiper.slideNext();
+    }
+  };
+
   const [products, setProducts] = useState([
     {
       id: 1,
-      img: "/Stanley.webp",
       title: "Stanley",
       description: "Quencher Recycled Stainless Steel Flowstate Tumbler, 1.18L",
-      price: "£44.99",
-      reference_price: "£55.6",
       rating: 5,
       ratingCount: 43,
-      colorCount: 4,
-
-      colors: ["#8fbc8f", "#f5f5dc", "#000", "#ffebcd"],
+      variants: [
+        {
+          vid: 1,
+          img: "/StanleyGreen.webp",
+          price: "£44.99",
+          reference_price: "£55.6",
+          color: "#8fbc8f",
+        },
+        {
+          vid: 2,
+          img: "/Stanley.webp",
+          price: "£45.99",
+          reference_price: "£55.6",
+          color: "#f5f5dc",
+        },
+        {
+          vid: 3,
+          img: "/StanleyBlack.webp",
+          price: "£48.99",
+          reference_price: "£55.6",
+          color: "#000",
+        },
+        {
+          vid: 4,
+          img: "/StanleyCream.webp",
+          price: "£47.99",
+          reference_price: "£55.6",
+          color: "#ffebcd",
+        },
+      ],
     },
 
     {
       id: 2,
-      img: "/Anyday.webp",
       title: "John Lewis ANYDAY",
       description: "Handheld & Foldable Desk Fan, 4 inch",
-      price: "£12.00",
-      reference_price: "£25.6",
       rating: 2,
       ratingCount: 1183,
-      colorCount: 4,
-
-      colors: ["#f0ffff", "#a9a9a9", "#6495ed", "#f0e68c"],
+      variants: [
+        {
+          vid: 1,
+          img: "/AnydayWhite.webp",
+          price: "£12.00",
+          reference_price: "£25.6",
+          color: "#f0ffff",
+        },
+        {
+          vid: 2,
+          img: "/AnydayGreen.webp",
+          price: "£13.00",
+          reference_price: "£25.6",
+          color: "#a9a9a9",
+        },
+        {
+          vid: 3,
+          img: "/AnydayBlue.webp",
+          price: "£14.00",
+          reference_price: "£25.6",
+          color: "#6495ed",
+        },
+        {
+          vid: 4,
+          img: "/Anyday.webp",
+          price: "£15.00",
+          reference_price: "£25.6",
+          color: "#f0e68c",
+        },
+      ],
     },
     {
       id: 3,
       img: "/Janerio.webp",
       title: "Sol de Janerio",
       description: "Perfume Mist Discovery Fragrance Gift Set",
-      price: "£44.99",
-      reference_price: "£55.6",
-      rating: 4,
+      rating: null,
       ratingCount: null,
-      colorCount: null,
-
-      colors: [""],
+      variants: [
+        {
+          vid: 1,
+          img: "/Janerio.webp",
+          price: "£40.00",
+          reference_price: "£50.00",
+          color: "",
+        },
+      ],
     },
     {
       id: 4,
-      img: "/Long.webp",
       title: "Longchamp",
       description: "Le Pliage Original Large Shoulder Bag",
-      price: "£115.00",
-      reference_price: "£155.6",
       rating: 5,
       ratingCount: 549,
-      colorCount: 6,
-
-      colors: ["#000", "#191970", "#faebd7", "#a9a9a9", "#5f9ea0", "#ff8c00"],
+      variants: [
+        {
+          vid: 1,
+          img: "/LongBlack.webp",
+          price: "£115.00",
+          reference_price: "£135.6",
+          color: "#000",
+        },
+        {
+          vid: 2,
+          img: "/LongBlue.webp",
+          price: "£115.00",
+          reference_price: "£135.6",
+          color: "#191970",
+        },
+        {
+          vid: 3,
+          img: "/LongCream.webp",
+          price: "£116.00",
+          reference_price: "£135.6",
+          color: "#faebd7",
+        },
+        {
+          vid: 4,
+          img: "/LongGrey.webp",
+          price: "£117.00",
+          reference_price: "£135.6",
+          color: "#a9a9a9",
+        },
+        {
+          vid: 4,
+          img: "/LongGreen.webp",
+          price: "£118.00",
+          reference_price: "£135.6",
+          color: "#5f9ea0",
+        },
+        {
+          vid: 4,
+          img: "/Long.webp",
+          price: "£119.00",
+          reference_price: "£135.6",
+          color: "#ff8c00",
+        },
+      ],
     },
     {
       id: 5,
-      img: "/John.webp",
       title: "John Lewis",
       description: "Aelia Bloom Maxi Dress",
-      price: "£65.00",
-      reference_price: "£75.6",
+
       rating: null,
       ratingCount: null,
-      colorCount: null,
-
-      colors: [""],
+      variants: [
+        {
+          vid: 1,
+          img: "/John.webp",
+          price: "£65.00",
+          reference_price: "£75.6",
+          color: "",
+        },
+      ],
     },
-
     {
       id: 1,
-      img: "/Stanley.webp",
       title: "Stanley",
       description: "Quencher Recycled Stainless Steel Flowstate Tumbler, 1.18L",
-      price: "£44.99",
-      reference_price: "£55.6",
       rating: 5,
       ratingCount: 43,
-      colorCount: 4,
-
-      colors: ["#8fbc8f", "#f5f5dc", "#000", "#ffebcd"],
+      variants: [
+        {
+          vid: 1,
+          img: "/StanleyGreen.webp",
+          price: "£44.99",
+          reference_price: "£55.6",
+          color: "#8fbc8f",
+        },
+        {
+          vid: 2,
+          img: "/Stanley.webp",
+          price: "£45.99",
+          reference_price: "£55.6",
+          color: "#f5f5dc",
+        },
+        {
+          vid: 3,
+          img: "/StanleyBlack.webp",
+          price: "£48.99",
+          reference_price: "£55.6",
+          color: "#000",
+        },
+        {
+          vid: 4,
+          img: "/StanleyCream.webp",
+          price: "£47.99",
+          reference_price: "£55.6",
+          color: "#ffebcd",
+        },
+      ],
     },
 
     {
       id: 2,
-      img: "/Anyday.webp",
       title: "John Lewis ANYDAY",
       description: "Handheld & Foldable Desk Fan, 4 inch",
-      price: "£12.00",
-      reference_price: "£25.6",
       rating: 2,
       ratingCount: 1183,
-      colorCount: 4,
-
-      colors: ["#f0ffff", "#a9a9a9", "#6495ed", "#f0e68c"],
+      variants: [
+        {
+          vid: 1,
+          img: "/AnydayWhite.webp",
+          price: "£12.00",
+          reference_price: "£25.6",
+          color: "#f0ffff",
+        },
+        {
+          vid: 2,
+          img: "/AnydayGreen.webp",
+          price: "£13.00",
+          reference_price: "£25.6",
+          color: "#a9a9a9",
+        },
+        {
+          vid: 3,
+          img: "/AnydayBlue.webp",
+          price: "£14.00",
+          reference_price: "£25.6",
+          color: "#6495ed",
+        },
+        {
+          vid: 4,
+          img: "/Anyday.webp",
+          price: "£15.00",
+          reference_price: "£25.6",
+          color: "#f0e68c",
+        },
+      ],
     },
     {
       id: 3,
       img: "/Janerio.webp",
       title: "Sol de Janerio",
       description: "Perfume Mist Discovery Fragrance Gift Set",
-      price: "£44.99",
-      reference_price: "£55.6",
-      rating: 3,
+      rating: null,
       ratingCount: null,
-      colorCount: null,
-
-      colors: [""],
+      variants: [
+        {
+          vid: 1,
+          img: "/Janerio.webp",
+          price: "£40.00",
+          reference_price: "£50.00",
+          color: "",
+        },
+      ],
     },
     {
       id: 4,
-      img: "/Long.webp",
       title: "Longchamp",
       description: "Le Pliage Original Large Shoulder Bag",
-      price: "£115.00",
-      reference_price: "£155.6",
       rating: 5,
       ratingCount: 549,
-      colorCount: 6,
-
-      colors: ["#000", "#191970", "#faebd7", "#a9a9a9", "#5f9ea0", "#ff8c00"],
+      variants: [
+        {
+          vid: 1,
+          img: "/LongBlack.webp",
+          price: "£115.00",
+          reference_price: "£135.6",
+          color: "#000",
+        },
+        {
+          vid: 2,
+          img: "/LongBlue.webp",
+          price: "£115.00",
+          reference_price: "£135.6",
+          color: "#191970",
+        },
+        {
+          vid: 3,
+          img: "/LongCream.webp",
+          price: "£116.00",
+          reference_price: "£135.6",
+          color: "#faebd7",
+        },
+        {
+          vid: 4,
+          img: "/LongGrey.webp",
+          price: "£117.00",
+          reference_price: "£135.6",
+          color: "#a9a9a9",
+        },
+        {
+          vid: 4,
+          img: "/LongGreen.webp",
+          price: "£118.00",
+          reference_price: "£135.6",
+          color: "#5f9ea0",
+        },
+        {
+          vid: 4,
+          img: "/Long.webp",
+          price: "£119.00",
+          reference_price: "£135.6",
+          color: "#ff8c00",
+        },
+      ],
     },
     {
       id: 5,
-      img: "/John.webp",
       title: "John Lewis",
       description: "Aelia Bloom Maxi Dress",
-      price: "£65.00",
-      reference_price: "£75.6",
+
       rating: null,
       ratingCount: null,
-      colorCount: null,
-
-      colors: [""],
+      variants: [
+        {
+          vid: 1,
+          img: "/John.webp",
+          price: "£65.00",
+          reference_price: "£75.6",
+          color: "",
+        },
+      ],
     },
   ]);
+
   return (
     <div className=" w-full mx-auto ">
       <div className="flex flex-col items-center justify-center gap-10  m-[10%] overflow-hidden relative min-w-[100px]">
         <strong className=" text-2xl">Popular products</strong>
-        <div className=" w-full">
+        <div className=" w-full z-0">
           <Swiper
-            navigation={true}
+            ref={swiperRef}
+            // navigation={true}
             modules={[Navigation]}
             className="mySwiper"
             slidesPerView={5}
-            spaceBetween={30}
+            slidesPerGroup={5}
+            spaceBetween={5}
             onSlideChange={() => console.log("slide change")}
             onSwiper={(swiper) => console.log(swiper)}
           >
@@ -160,21 +365,29 @@ export default function ProductCarousal() {
               <>
                 <SwiperSlide>
                   <Product
+                    variants={product.variants}
                     rating={product.rating}
                     id={product.id}
-                    img={product.img}
                     title={product.title}
                     description={product.description}
-                    price={product.price}
-                    reference_price={product.reference_price}
                     ratingCount={product.ratingCount}
-                    colors={product.colors}
-                    colorCount={product.colorCount}
                   />
                 </SwiperSlide>
               </>
             ))}
           </Swiper>
+          <button
+            onClick={handlePrev}
+            className="absolute top-0 left-0 translate-x-0 translate-y-64 z-20 border px-1 py-1 rounded-full bg-slate-400 hover:bg-slate-300 "
+          >
+            <GoChevronLeft size="3rem" />
+          </button>
+          <button
+            onClick={handleNext}
+            className="absolute top-0 right-0 translate-x-0 translate-y-64 z-20 border px-1 py-1 rounded-full bg-slate-400 hover:bg-slate-300   "
+          >
+            <GoChevronRight size="3rem" />
+          </button>
         </div>
       </div>
     </div>
--- components/reusable/product.js
+++ components/reusable/product.js
@@ -1,81 +1,62 @@
 import { useState } from "react";
 import { MdStar } from "react-icons/md";
-import Link from "next/link";
 
 export default function Product(props) {
-  const {
-    id,
-    img,
-    title,
-    description,
-    price,
-    reference_price,
-    rating,
-    ratingCount,
-    colorCount,
-    colors,
-  } = props;
-
-  const [isHovering, setIsHovering] = useState(false);
-  const handleMouseOver = () => {
-    setIsHovering(true);
-  };
-
-  const handleMouseOut = () => {
-    setIsHovering(false);
-  };
-
-  const hoverText = () => {
-    return (
-      <Link
-        href="https://www.johnlewis.com/"
-        className="px-3 py-2 border rounded-sm text-center border-black bg-white absolute translate-x-4 translate-y-60 delay-300 duration-300 w-[200px] transition-all animate-pulse"
-      >
-        Quick view
-      </Link>
-    );
-  };
+  const { id, title, description, rating, ratingCount, variants } = props;
+  const [activeVariant, setActiveVariant] = useState(variants[0]);
 
   return (
     <>
-      {isHovering && hoverText()}
       <div
         key={id}
-        className=" lg:w-[230px] mx-auto flex flex-col items-start justify-center "
-        onMouseOver={handleMouseOver}
-        onMouseOut={handleMouseOut}
+        className=" lg:w-[230px] mx-auto flex flex-col items-start justify-center group relative "
       >
-        <img src={img} alt="" />
-        <button className="text-white bg-black px-3 py-2 mx-4 rounded-sm w-[200px] hover:bg-green-500 hover:text-black">
+        <img src={activeVariant.img} alt="" />
+
+        <a href="https://www.johnlewis.com/">
+          <button className="px-3 py-2 border rounded-sm text-center border-black bg-white absolute top-0 left-0 translate-x-4 translate-y-72 duration-200 delay-200 w-[200px] transition-all  invisible ease-in-out group-hover:visible group-hover:translate-y-64 hover:border-green-500 font-semibold">
+            Quick view
+          </button>
+        </a>
+
+        <button className="text-white bg-black px-3 py-2 mx-4 rounded-sm w-[200px] hover:bg-green-500 hover:text-black font-semibold">
           Add to basket
         </button>
+
         <h1 className="mx-4 text-left pt-4"> {title}</h1>
+
         <p className="text-slate-500 px-4 py-3 border-b w-[218px]">
           {description}
         </p>
+
         <span className=" font-bold border-b px-4 py-3 w-[218px] text-green-500">
-          {price}
+          {activeVariant.price}
         </span>
+
         <span className=" font-bold border-b px-4 py-3 w-[218px] text-red-500">
-          {reference_price}
+          {activeVariant.reference_price}
         </span>
+
         <div className="flex items-center justify-start px-4 py-3 border-b w-[218px]">
           {Array.from({ length: rating }, (i) => (
             <MdStar />
           ))}
+
           <span>{ratingCount}</span>
         </div>
 
-        <></>
-      </div>
-      <div className="flex items-center justify-start p-4 gap-2">
-        {Array.from({ length: colorCount }, (i) => (
-          <button
-            className={`px-2 py-2 border border-slate-400 rounded-full ring ring-slate-300 hover:ring-offset-1 bg-[${colors.map(
-              (color, i) => `color`
-            )}] `}
-          ></button>
-        ))}
+        <div className="flex items-center justify-start p-4 gap-2">
+          {variants.map((variant) => (
+            <button
+              onClick={() => setActiveVariant(variant)}
+              key={variant.vid}
+              className={`px-2 py-2 border border-slate-400 rounded-full ring ring-slate-300 hover:ring-offset-1 
+           
+             `}
+              style={{ backgroundColor: variant.color }}
+            ></button>
+          ))}
+        </div>
       </div>
     </>
   );
